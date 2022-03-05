22575051f4d84e41916497418ec648663d3c70d9
Kim Dung Nguyen <nguyenkimdung7896@gmail.com>
2022-03-05 16:45:41 +0900

    demo

--- pironapp/urls.py
+++ pironapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile_action)
+    path('profile/', views.profile_action),
+    path('demo/edit', views.demo_action)
 ]
--- pironapp/views.py
+++ pironapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def profile_action(request):
     return render(request, 'profile.html')
+
+def demo_action(request):
+    return render(request, 'demo.html')
+
+def login_action(request):
+    return render(request, 'login.html')
