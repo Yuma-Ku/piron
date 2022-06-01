3829029e9eb8dba0cbaaf1131430aba086b57070
Benson Urcia <benson.urcia@gmail.com>
2022-06-01 22:13:05 +0900

    ・Faviconを追加 ・general.cssを新規作成 ・ログイン画面を調整（途中） ・.gitignoreに場所を追加

--- pironapp/urls.py
+++ pironapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('profile/', views.profile_action),
     path('post/',views.post_action),
-    path('login/', views.login_action)
+    path('login/', views.login_action),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("images/favicon.ico")))
 ]
 
  
